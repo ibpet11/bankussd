cfa72f60792ae7afc03b25e85734e4dae0632630
James Amo <james.amo@outlook.com>
2018-03-26 15:04:36 +0000

    Changes to the settings file
    removing all the settings to the .env file.

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,9 +1,9 @@
 export default {
-  apikey: '9f925b2e-10c7-46cb-b2b9-fddceb05f2fd',
-  secret: 'osp2a0xhny',
-  loginurl: 'http://52.24.33.201/login',
-  paymentUrl: 'http://52.24.33.201/api/payment',
-  infobipSmsUrl: 'https://api.infobip.com/sms/1/text/single',
-  infobipUser: 'OneForAll',
-  infobipPass: 'Password1',
+  apikey: process.env.API_APIKEY,
+  secret: process.env.API_SECRET,
+  loginurl: `${process.env.VEEPAY_URL_BASE}/login`,
+  paymentUrl: `${process.env.VEEPAY_URL_BASE}/api/payment`,
+  infobipSmsUrl: process.env.INFO_BI_SMS_URL,
+  infobipUser: process.env.INFO_BI_SMS_USER,
+  infobipPass: process.env.INFO_BI_SMS_PASSWORD,
 };
